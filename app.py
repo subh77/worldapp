59a3f8df3305b1154eaf44d9cfebc558cfda70fd
subh77 <subhro77@outlook.com>
2021-05-23 17:04:53 +0000

    Modified webpages Added new parent 'layout.html' and moved homepage and about pages to child processes. Overridden parent body content with child body content. Also added bootstarp and css for styling.

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Steven Clive',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'dateposted': 'April 20, 2021'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'dateposted': 'April 21, 2018'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def homepage():
-    return render_template('home.html')
+    return render_template('home.html', authpost=posts, title='Blog Homepage')
 
 @app.route('/about')
 def about():
-    return "About page!?"
+    return render_template('about.html', title='Blog About Page')
 
 if __name__ == "__main__":
     app.run(debug=True)
